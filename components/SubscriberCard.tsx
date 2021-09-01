08bbb2ba74e084f694592114ceb0d1d7ac5352e4
Hrishi <hrishivo@usc.edu>
2021-08-31 19:53:09 -0700

    changes

--- components/SubscriberCard.tsx
+++ components/SubscriberCard.tsx
@@ -97,6 +97,8 @@ const styles: any = (status: any) => StyleSheet.create({
         borderRadius: 15,
         padding: 13,
         backgroundColor: '#F8F9FA',
+        borderWidth: 1,
+        borderColor: '#eeeeee'
     },
     text: {
         display: 'flex',
