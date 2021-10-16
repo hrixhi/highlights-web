7a39edb397b1c845f5f06bfd90412e340a963719
Prabir Vora <pvora30@gatech.edu>
2021-10-16 19:09:32 -0400

    JWT frontend

--- graphql/FetchAPI.ts
+++ graphql/FetchAPI.ts
@@ -1,14 +1,41 @@
 import ApolloClient from "apollo-boost";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const fetchAPI = (userId: any) => {
-  // LIVE
   const uri = "https://api.cuesapp.co/";
-  // DEV
   // const uri = "http://localhost:8081/";
+
   return new ApolloClient({
     uri,
     headers: {
       userId
-    }
+    },
+    fetchOptions: {
+      credentials: 'include'
+    },
+    request: async (operation) => {
+      const token = await AsyncStorage.getItem('jwt_token')
+      operation.setContext({
+        headers: {
+          authorization: token || ""
+        }
+      });
+    },
+    onError: ({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        for (let err of graphQLErrors) {
+          if (err.message === "NOT_AUTHENTICATED") {
+            alert("Session Timed out. You will be logged out.")
+            AsyncStorage.clear();
+            window.location.reload();
+            return;
+          }
+        }
+      }
+      if (networkError) {
+        // logoutUser();
+        console.log(networkError)
+      }
+    },
   });
 };
